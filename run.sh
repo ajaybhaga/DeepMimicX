2ca84659655b2e17dd99c40377f89cc79f8d9c49
Ajay Bhaga <ajay.bhaga@gmail.com>
2020-03-03 16:21:05 -0500

    Added baseline source code.

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+LD_LIBRARY_PATH=/usr/local/lib/:/usr/local/cuda-10.1/lib64:/usr/lib/x86_64-linux-gnu/
+export LD_LIBRARY_PATH
+
+python -m pdb DeepMimic.py --arg_file args/run_humanoid3d_spinkick_args.txt
