5e34625fbf96a51e21ab5149f974b02eced04a79
Ajay Bhaga <ajay.bhaga@gmail.com>
2020-03-09 15:08:46 -0400

    Updated run.sh to use ipython.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 LD_LIBRARY_PATH=/usr/local/lib/:/usr/local/cuda-10.1/lib64:/usr/lib/x86_64-linux-gnu/
 export LD_LIBRARY_PATH
 
-python -m pdb DeepMimic.py --arg_file args/run_humanoid3d_spinkick_args.txt
+ipython3 --pdb DeepMimic.py -- arg_file args/run_humanoid3d_spinkick_args.txt
